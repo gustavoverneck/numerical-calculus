04bc35243c7370097170f686ccb8f5f80901c23f
Gustavo A. Verneck <gustavoverneck@gmail.com>
2023-07-27 00:45:58 -0300

    Added diagonalization

--- solvers.f90
+++ solvers.f90
@@ -26,59 +26,69 @@ module  numerical_recipes
     end function BhaskaraSolver
 
 !   -------------------------------------------------
-!   Gauss-Jordan solver
 !   Linearizes Matrix - Solves System of equations
-!   FAZER
-    function gaussJordan(a)
+    function diagonalize(x)
         implicit none
         
-        integer :: i, j, k
-        real, dimension(:, :), intent(in) :: a
-        real :: n, gaussJordan
+        integer :: i, j, k, n, p
+        real, dimension(:, :), intent(in) :: x
+        real, dimension(size(x, 1), size(x, 2)) :: a
+        real, dimension(size(x, 1)) :: diagonalize
+        real :: m
 
-        n = size(a)
-        gaussJordan = n
+        n = size(x, 1)  ! n rows
+        a = x
+         
+        ! Zerar triangulo superior
+        do j = n, 1, -1
+            if (j .GE. 2) then
+                do i = j-1, 1, -1
+                    m = a(i, j) / a(j, j)
+                    do k = 1, n+1, 1
+                        a(i, k) = a(i, k) - m*a(j, k)
+                    end do
+                end do
+            end if
+        end do
+
+        ! Zerar triângulo inferior
+        do j = 1, n, 1  ! para cada linha (elemento da diagonal)
+            if (j .LE. n-1) then
+                do i = j+1, n, 1
+                    m = a(i, j) / a(j, j)
+                    do k = 1, n+1 , 1
+                        a(i, k) = a(i, k) - m*a(j, k)
+                    end do
+                end do
+            end if
+        end do
+
+        ! Adequar diagonal principal
+        do j = 1, n, 1
+            m = a(j, j)
+            do k = 1, n+1, 1
+                a(j, k) = a(j, k) / m
+            end do
+        end do
+        diagonalize = a(n + 1, :)
         return
-    end function gaussJordan
+    end function diagonalize
 
+end module numerical_recipes
 !   -------------------------------------------------
 !   FAZER
 !   Cubic Spline Interpolation
-    function cubicSpline()
-        implicit none
-        return
-    !declarar variaveis
-    
-    end function cubicSpline
 
 !   -------------------------------------------------
 !   FAZER (Numerical Recipes páginas 382~286)
 !   Broyden's Method
 !   Solves non-linear systems of equations
-    function broyden(x)
-        implicit none
-        return
-    end function broyden
 
 !   -------------------------------------------------
 !   FAZER
 !   Integration
 !   ADICIONAR MÉTODO DE INTEGRAÇÃO EM 1 OU MAIS DIMENSÕES
-    function integrate(x)
-        implicit none
-        return
-    end function integrate
-
 
 !   -------------------------------------------------
 !   FAZER
 !   Fast Fourier Transform
-    function fft()
-        implicit none
-        return
-    end function fft
-
-
-
-
-end module solvers
